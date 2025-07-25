4db6872a1674e095f39594670b0b3e6a4dd1f1a7
pablopolo87 <44262148+pablopolo87@users.noreply.github.com>
2025-07-25 20:40:58 +0200

    Update emitir.sh

--- emitir.sh
+++ emitir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ffmpeg -re \
--loop 1 -framerate 2 -i fondo.png \
+-loop 1 -framerate 2 -i fondo.jpg \
 -f concat -safe 0 -i playlist.txt -shortest \
 -c:v libx264 -preset ultrafast -tune stillimage -b:v 900k -pix_fmt yuv420p \
 -c:a aac -b:a 128k -ar 44100 \
