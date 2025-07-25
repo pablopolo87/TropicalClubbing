8b4b454731baa6ef8f34860ddb3896c0ddae725d
pablopolo87 <44262148+pablopolo87@users.noreply.github.com>
2025-07-25 20:54:37 +0200

    Update emitir.sh

--- emitir.sh
+++ emitir.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# Clave de emisión de YouTube (reemplazar por la suya si no está en variable)
+STREAM_URL="rtmp://a.rtmp.youtube.com/live2"
+STREAM_KEY="05fu-mqjb-zca8-6d3c-e9xx"  # <- Su clave RTMP
+
+# Generar lista aleatoria de canciones
+find canciones/ -type f -name "*.mp3" > playlist.txt
+shuf playlist.txt > playlist_loop.txt
+
+# Emitir fondo.jpg con música en bucle
 ffmpeg -re \
--loop 1 -framerate 2 -i fondo.jpg \
--f concat -safe 0 -i playlist.txt -shortest \
--c:v libx264 -preset ultrafast -tune stillimage -b:v 900k -pix_fmt yuv420p \
--c:a aac -b:a 128k -ar 44100 \
--f flv "rtmp://a.rtmp.youtube.com/live2/05fu-mqjb-zca8-6d3c-e9xx"
+  -loop 1 -framerate 2 -i fondo.jpg \
+  -f concat -safe 0 -i <(for f in $(cat playlist_loop.txt); do echo "file '$PWD/$f'"; done) \
+  -c:v libx264 -preset veryfast -tune stillimage -pix_fmt yuv420p \
+  -c:a aac -b:a 160k -ar 44100 \
+  -f flv "$STREAM_URL/$STREAM_KEY"
