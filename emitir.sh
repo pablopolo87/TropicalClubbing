53b594e9cab1fac502245a5f455b59f7e5b2bb69
pablopolo87 <44262148+pablopolo87@users.noreply.github.com>
2025-07-25 20:31:57 +0200

    Update emitir.sh

--- emitir.sh
+++ emitir.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 ffmpeg -re \
--loop 1 -framerate 2 -i fondo.png \
--stream_loop -1 -f concat -safe 0 -i playlist.txt \
--shortest \
--c:v libx264 -preset veryfast -tune stillimage -b:v 2500k \
+-loop 1 -framerate 2 -i fondo.jpg \
+-f concat -safe 0 -i playlist.txt -shortest \
+-c:v libx264 -preset ultrafast -tune stillimage -b:v 900k -pix_fmt yuv420p \
 -c:a aac -b:a 128k -ar 44100 \
 -f flv "rtmp://a.rtmp.youtube.com/live2/05fu-mqjb-zca8-6d3c-e9xx"
